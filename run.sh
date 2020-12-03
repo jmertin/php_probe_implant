3c0a334738bb3412c5bec74b8bf97d20eefa65c9
Joerg Mertin <joerg.mertin@ca.com>
2020-12-03 16:30:44 +0100

    Adapted run script to kubernetes initContainer deployment scheme.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 #
 # In case we have an ENTRYPOINT?CMD defined, use that one instead
@@ -7,29 +7,35 @@ then
     # Deplpoy the probe.
     if [ "$PHP_DEPLOY" = "true" ]
     then
-	if [ -x ${PHP_PROBE_DIR:-/opt/apmia}/php-probe.sh ]
-	then
-	    echo " * Implanting PHP probe"
-	    # Execute php-probe.sh
-	    /bin/sh ${PHP_PROBE_DIR:-/opt/apmia}/php-probe.sh
-	else
-	    echo "*** FATAL: PHP deployment enabled, but probe-installer not found. Exiting!"
-	fi
+        if [ -x /opt/apmia/php-probe.sh ]
+        then
+            echo " * Implanting PHP probe"
+            # Execute php-probe.sh
+            /bin/sh /opt/apmia/php-probe.sh
+        else
+            echo "*** FATAL: PHP deployment enabled, but probe-installer not found. Exiting!"
+        fi
     fi
     
     if [ ! -f $PHP_LOGDIR ]
     then
-	mkdir -p $PHP_LOGDIR
+        mkdir -p $PHP_LOGDIR
         # If you want to have logs, identify the user the Web-Server
-	# is running as, and apply the chmod accordingly.
-	## chown www-data.www-data $PHP_LOGDIR
-	chmod 775 $PHP_LOGDIR
+        # is running as, and apply the chmod accordingly.
+        chown ${PHP_WWWMODE}.${PHP_WWWMODE} $PHP_LOGDIR
+        chmod 775 $PHP_LOGDIR
     fi
     
     echo " * Handing over to real ENTRYPOINT: $ENTRYPOINT"
     # Execute real Entrypoint. Make sure it hooks itself to the shell.
-    $ENTRYPOINT
+    exec $ENTRYPOINT
 else
+    echo "*** php-probe implant entry point"
+    echo "*** Current directory: `pwd`"
+    echo "Copying over all files from /data/apmia directory to /opt/apmia"
+    cp -ar /opt/data/apmia /opt/
+    cp -ar /opt/data/*.sh /opt/apmia/ 
+    chmod 755 /opt/apmia /opt/apmia/*.sh
     # Exit with a true status
     /bin/true
 fi
